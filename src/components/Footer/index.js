281d212407d7cc36286db6edf8937030437352a7
Ajay <athota766@gmail.com>
2023-01-23 13:47:16 +0000

    Movies amazon

---
+++ src/components/Footer/index.js
@@ -0,0 +1,24 @@
+import {
+  FaGoogle,
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+  FaYoutube,
+} from 'react-icons/fa'
+
+import './index.css'
+
+const Footer = () => (
+  <div className="footer-page">
+    <div className="contact-icon-container">
+      <FaGoogle className="contact-icon" />
+      <FaFacebook className="contact-icon" />
+      <FaTwitter className="contact-icon" />
+      <FaInstagram className="contact-icon" />
+      <FaYoutube className="contact-icon" />
+    </div>
+    <p className="contact-text">Contact us</p>
+  </div>
+)
+
+export default Footer
